84bee923c4e1adbf6e3bab59cdbb5be0a8a79d6a
Connor Goodwill <goodwill.connor@gmail.com>
2024-12-11 23:00:10 +0100

    fix imports, remove broken dependency, controller & basic index ui

--- src/main/kotlin/com/example/SportsBookingSystem/Controller/IndexController.kt
+++ src/main/kotlin/com/example/SportsBookingSystem/Controller/IndexController.kt
@@ -1,4 +1,13 @@
 package com.example.SportsBookingSystem.Controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class IndexController {
+
+    @GetMapping("/")
+    fun index(): String {
+        return "index"
+    }
 }
--- src/main/kotlin/com/example/SportsBookingSystem/Entity/AdvertEntity.kt
+++ src/main/kotlin/com/example/SportsBookingSystem/Entity/AdvertEntity.kt
@@ -3,22 +3,19 @@ package com.example.SportsBookingSystem.Entity
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
-import org.springframework.data.annotation.Id
+import jakarta.persistence.Id
 
 @Entity
 class AdvertEntity (
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        var id: Long?,
         var adtype: String,
         var hyperlink: String,
         var matchId: Long?,
         var teamId: Long?,
         var tournamentId: Long?,
-        var status: String
-)
+        var status: String,
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Long?=null
+        )
 
-{
-
-}
 
